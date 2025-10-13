67109b341a084e75a8d3c6ff93933ad7d14db9f7
andre-gcosta <andre.gcosta1@gmail.com>
2025-10-13 20:19:37 -0300

    alteração app module para não servir a pasta estatica para rotas /api

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    // Serve o frontend React
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', '..', 'frontend', 'dist'),
+      exclude: ['/api*'], // não serve a pasta estática para rotas /api
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })

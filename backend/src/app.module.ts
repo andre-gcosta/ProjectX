adbcd689ec0fab5e800aa6f10d4ef1c3dd8c0684
andre-gcosta <andre.gcosta1@gmail.com>
2025-10-13 21:59:11 -0300

    alterações ok localmente monolito porta 3000

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -7,7 +7,7 @@ import { AppService } from './app.service';
 @Module({
   imports: [
     ServeStaticModule.forRoot({
-      rootPath: join(__dirname, '..', '..', 'frontend/dist'), // build do frontend
+      rootPath: join(__dirname, '../../frontend/dist'), // build do frontend
       exclude: ['/api*'] // backend continua funcionando em /api
     }),
   ],

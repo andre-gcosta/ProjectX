34d7ac7f8f21595b2ad59d56d7eb69cd0565ed79
andre-gcosta <andre.gcosta1@gmail.com>
2025-10-16 01:36:43 -0300

    removido nestexpress

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,11 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
-import { NestExpressApplication } from '@nestjs/platform-express';
+
 import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  const app = await NestFactory.create(AppModule);
 
   const allowedOrigins = (process.env.CORS_ORIGIN || '').split(',');
 
